6f51c8cf93d810ebc96d8269455783d4e4ff7bc4
sindhu718 <vagasindhu718@gmail.com>
2024-11-22 00:49:17 -0500

    test function for division

--- main_test.go
+++ main_test.go
@@ -4,6 +4,28 @@ import (
 	"testing"
 )
 
+func TestDivide(t *testing.T) {
+	//error handling for wrong answer
+	resultOne, _ := Divide(4, 2)
+	expected := 2
+
+	if resultOne != 2 {
+		t.Errorf("Expected %d, got %d", expected, resultOne)
+	}
+
+	//error handling for divisible by 0 answer
+	_, err := Divide(3, 0)
+	if err == nil {
+		t.Errorf("Expected an error when dividing by zero, got nil")
+	}
+
+	expectedError := "division by zero"
+
+	if err.Error() != expectedError {
+		t.Errorf("Expected error message '%s', got '%s'", expectedError, err.Error())
+	}
+
+}
 func TestSquare(t *testing.T) {
 	result := Square(5)
 	expected := 25
