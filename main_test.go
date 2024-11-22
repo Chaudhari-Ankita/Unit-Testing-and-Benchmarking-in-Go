c64faef5d549fa2ac0f8e8d5ecb7d8c9c7a5d989
Chaudhari-Ankita <chaudhariankita1410@gmail.com>
2024-11-22 00:46:05 -0500

    Test Function for Square operation

--- main_test.go
+++ main_test.go
@@ -4,6 +4,14 @@ import (
 	"testing"
 )
 
+func TestSquare(t *testing.T) {
+	result := Square(5)
+	expected := 25
+	if result != expected {
+		t.Fatalf("Square(5) will result %d, but we got %d", result, expected)
+	}
+}
+
 func BenchmarkDivide(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Divide(100, 5)
